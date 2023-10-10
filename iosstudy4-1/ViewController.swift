5afac60dadc4dfac83cb7f67a6fdf7524e4cb0e2
s01ver <solverglace@gmail.com>
2023-10-10 21:08:30 +0900

    Initial Commit

---
+++ iosstudy4-1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  iosstudy4-1
+//
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var plusButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        plusButton.tintColor = .orange
+    }
+
+
+}
+
